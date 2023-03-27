389027c2b6736f06f8365c4e3de145dc613606d7
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2023-03-28 06:09:13 +0700

    section 8 done but...
    SOME BUG THAT I DONT KNOW HOW IT'S HAPPEND, AFTER SET EXPENSESSTATE, ALL EXPENSES'S DATE HAS CHANGED TO SPESIFIC DATE AND I DONT KNOW WHY IT S HAPPEND

---
+++ screens/AllExpenses.js
@@ -0,0 +1,16 @@
+import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput";
+import { useContext } from "react";
+import { ExpensesContext } from "../store/expenses.contex";
+
+function AllExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod={"Total"}
+      fallbackText={"No registered expenses found!"}
+    />
+  );
+}
+export default AllExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput";
+import { useContext } from "react";
+import { ExpensesContext } from "../store/expenses.contex";
+import { getDateMinusDays } from "../util/date";
+function RecentExpenses() {
+  const expensesCtx = useContext(ExpensesContext)
+  const recentExpenses = expensesCtx.expenses.filter((expense) =>{
+    const today = new Date()
+    const date7DaysAgo = getDateMinusDays(today,7)
+
+    return (expense.date = date7DaysAgo) && (expense.date <= today)
+
+  })
+
+  return (
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod={'Last 7 Days'} fallbackText={'No Expenses registered for the last 7 days.'}/>
+  );
+}
+export default RecentExpenses;
